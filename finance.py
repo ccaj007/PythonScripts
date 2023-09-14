ec9e27418688f589622df0fc9fdfc1e194b6438f
Ginger Mei <ccaj007@gmail.com>
2023-09-13 20:43:25 -0400

    update

--- finance.py
+++ finance.py
@@ -3,24 +3,45 @@ import yfinance as yf
 import datetime
 import requests
 
-# nvda = yf.Ticker("NVDA")
-# stockinfo = nvda.info
 df = pd.DataFrame()
 
-tickers = ['AMZN', 'NEE', 'CSCO', 'DMTK']
-for ticker in tickers:
-    data = yf.Ticker(ticker)
-    price = data.info['currentPrice']
+# tickers = ['AMZN', 'NEE', 'CSCO', 'DMTK']
+# for ticker in tickers:
+#     data = yf.Ticker(ticker)
+#     price = data.info['currentPrice']
+
+#     new_row = {
+#         "ticker": ticker,
+#         "price": price
+#     }
+
+#     df = df.append(new_row, ignore_index=True)
+
+# print(df)
+
+portfolio = pd.read_csv('F:\Documents\Finance\Portfolio_raw.csv')
+sum = 0
+for index,row in portfolio.iterrows():
+    data = yf.Ticker(row['Ticker'])
+    price = data.info['regularMarketPreviousClose']
+    value = price * row['Shares']
+    sum += value
 
     new_row = {
-        "ticker": ticker,
-        "price": price
+        "CompanyName": data.info['shortName'],
+        "Ticker": row['Ticker'],
+        "Shares": row['Shares'],
+        "Price": price,
+        "Value": value
     }
 
     df = df.append(new_row, ignore_index=True)
+    #print(data.info['shortName'],row['Ticker'],price, value)
 
+new_row = {
+    "CompanyName": "TOTAL",
+    "Total": int(sum)
+}
+df = df.append(new_row, ignore_index=True)
+df.to_csv("PortfolioValue.csv", index = False)
 print(df)
-# for key,value in stockinfo.items():
-#     print(key, ":", value)
-
-# print(stockinfo['currentPrice'])
